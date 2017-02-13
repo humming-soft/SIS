9a332aabc24d1044323da833e6459d99365dcffa
agailevictor <agailevictor@gmail.com>
2017-02-13 10:52:45 +0800

    Signed-off-by: agailevictor <agailevictor@gmail.com>

---
+++ SIS2.0/SIS_V/hq/hq_master.Master.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SIS_V.hq
+{
+    public partial class hq_master : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
