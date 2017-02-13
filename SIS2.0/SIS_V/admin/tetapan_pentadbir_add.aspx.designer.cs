23d3e399d74fa514217e3ac3eb5e223c2c131e29
ancymathew <ancy@hummingsoft.com.my>
2017-02-13 12:31:40 +0800

    Signed-off-by: ancymathew <ancy@hummingsoft.com.my>

--- SIS2.0/SIS_V/admin/tetapan_pentadbir_add.aspx.designer.cs
+++ SIS2.0/SIS_V/admin/tetapan_pentadbir_add.aspx.designer.cs
@@ -58,6 +58,15 @@ namespace SIS_V.admin {
         protected global::System.Web.UI.WebControls.DropDownList DropDownList1;
         
         /// <summary>
+        /// DropDownList2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DropDownList2;
+        
+        /// <summary>
         /// TextBox5 control.
         /// </summary>
         /// <remarks>
