671c1809eb451a853db8646954e8a0d02dc6da98
janeelizabethjose <jane@hummingsoft.com.my>
2017-02-14 11:13:25 +0800

    Signed-off-by: janeelizabethjose <jane@hummingsoft.com.my>

---
+++ SIS2.0/SIS_V/state/activiti_parti_view.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using SIS_B;
+
+namespace SIS_V.state
+{
+    public partial class activiti_parti_view : System.Web.UI.Page
+    {
+        bus_sis_ugc1 bus = new bus_sis_ugc1();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                fill_DataTable();
+            }
+        }
+
+        public void fill_DataTable()
+        {
+            DataTable dt = bus.fill_DataTable();
+            if (dt.Rows.Count > 0)
+            {
+                GridDataTable2.DataSource = dt;
+                GridDataTable2.DataBind();
+            }
+        }
+        protected void GridDataTable2_PreRender(object sender, EventArgs e)
+        {
+            if (GridDataTable2.Rows.Count > 0)
+            {
+                GridDataTable2.UseAccessibleHeader = true;
+                GridDataTable2.HeaderRow.TableSection = TableRowSection.TableHeader;
+            }
+        }
+    }
+}
