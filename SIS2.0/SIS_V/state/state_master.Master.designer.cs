6ff4a1828a2edce5aec6c16ec6c1e2f8dc37913a
unknown <Sebin@HS-PC07.Office.Hummingsoft.com.my>
2017-02-17 13:45:56 +0800

    Signed-off-by: unknown <Sebin@HS-PC07.Office.Hummingsoft.com.my>

--- SIS2.0/SIS_V/state/state_master.Master.designer.cs
+++ SIS2.0/SIS_V/state/state_master.Master.designer.cs
@@ -76,6 +76,15 @@ namespace SIS_V.state {
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl tetapan;
         
         /// <summary>
+        /// teta control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl teta;
+        
+        /// <summary>
         /// form1 control.
         /// </summary>
         /// <remarks>
