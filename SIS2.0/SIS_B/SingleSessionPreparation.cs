636b168840a66e6bb97ff24c8c96c8d569b488a9
unknown <Sebin@HS-PC07.Office.Hummingsoft.com.my>
2017-02-22 17:30:09 +0800

    Signed-off-by: unknown <Sebin@HS-PC07.Office.Hummingsoft.com.my>

---
+++ SIS2.0/SIS_B/SingleSessionPreparation.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Security;
+using SIS_D;
+
+namespace SIS_B
+{
+    public static class SingleSessionPreparation
+    {
+        /// <summary>
+        /// Called during LoggedIn event. Need to pass username
+        /// as login process not fully completed
+        /// </summary>
+        public static void CreateAndStoreSessionToken(string userName)
+        {
+            // Will be using the response object several times
+            HttpResponse pageResponse = HttpContext.Current.Response;
+
+            // 'session' token
+            Guid sessionToken = System.Guid.NewGuid();
+
+            // Get authentication cookie and ticket
+            HttpCookie authenticationCookie =
+                pageResponse.Cookies[FormsAuthentication.FormsCookieName];
+            FormsAuthenticationTicket authenticationTicket =
+                FormsAuthentication.Decrypt(authenticationCookie.Value);
+
+            // Create a new ticket based on the existing one that includes the 'session' token in the userData
+            FormsAuthenticationTicket newAuthenticationTicket =
+                new FormsAuthenticationTicket(
+                authenticationTicket.Version,
+                authenticationTicket.Name,
+                authenticationTicket.IssueDate,
+                authenticationTicket.Expiration,
+                authenticationTicket.IsPersistent,
+                sessionToken.ToString(),
+                authenticationTicket.CookiePath);
+
+            // Store session token in Membership comment
+            // You may want to store other information in the comment
+            // field, if so, you may have to implement some dilimited
+            // structure within it, perhaps xml
+            SisUser currentUser = SisUserService.getUser(userName);
+            currentUser.refToken = sessionToken.ToString();
+            SisUserService.updateUser(currentUser);
+            //MembershipUser currentUser = Membership.GetUser(userName);
+            //currentUser.Comment = sessionToken.ToString();
+            //Membership.UpdateUser(currentUser);
+
+            // Replace the authentication cookie
+            pageResponse.Cookies.Remove(FormsAuthentication.FormsCookieName);
+
+            HttpCookie newAuthenticationCookie = new HttpCookie(
+                FormsAuthentication.FormsCookieName,
+                FormsAuthentication.Encrypt(newAuthenticationTicket));
+
+            newAuthenticationCookie.HttpOnly = authenticationCookie.HttpOnly;
+            newAuthenticationCookie.Path = authenticationCookie.Path;
+            newAuthenticationCookie.Secure = authenticationCookie.Secure;
+            newAuthenticationCookie.Domain = authenticationCookie.Domain;
+            newAuthenticationCookie.Expires = authenticationCookie.Expires;
+
+            pageResponse.Cookies.Add(newAuthenticationCookie);
+        }
+    }
+}
---
+++ SIS2.0/SIS_D/SisUserService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace SIS_D
+{
+    public class SisUserService
+    {
+        private static int tokenId;
+        private static String username;
+        private static String token;
+        private static DateTime timeStamp;
+        public static SisUser getUser(String userName)
+        {
+            data_sis_ugc4 data = new data_sis_ugc4();
+            DataTable tokenInfo = new DataTable();
+            tokenInfo = data.data_get_token(userName);
+            foreach (DataRow row in tokenInfo.Rows)
+            {
+                tokenId = Convert.ToInt32(row["tokenid"].ToString());
+                username = row["username"].ToString();
+                token = row["token"].ToString();
+                timeStamp = Convert.ToDateTime(row["timestamp"]);
+            }
+            return new SisUser(tokenId, userName, token, timeStamp);
+        }
+        public static void updateUser(SisUser user)
+        {
+            data_sis_ugc4 data = new data_sis_ugc4();
+            int status = data.data_set_token(user);
+        }
+    }
+}
