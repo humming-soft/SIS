7967c41239f890103430d897d56fcd1f2c33e0e5
agailevictor <agailevictor@gmail.com>
2017-02-27 12:59:19 +0800

    Signed-off-by: agailevictor <agailevictor@gmail.com>

--- SIS2.0/SIS_V/state/Win_Candidate.aspx.cs
+++ SIS2.0/SIS_V/state/Win_Candidate.aspx.cs
@@ -16,23 +16,8 @@ namespace SIS_V.state
 
         }
         [WebMethod]
-        //public static List<Parliment_WC> parlimen_w(int sid)
-        //{
-        //    List<Parliment_WC> parlimen_w = new List<Parliment_WC>();
-        //    bus_sis_ugc1 buspar = new bus_sis_ugc1();
-        //    buspar.stateid = sid;
-        //    DataTable dt = buspar.parlimen();
-        //    for (int i = 0; i < dt.Rows.Count; i++)
-        //    {
-        //        Parliment_WC _ParlimentW = new Parliment_WC();
-        //        _ParlimentW.Color_code = int.Parse(dt.Rows[i]["analysis_color_id"].ToString());
-        //        _ParlimentW.Color_Value = dt.Rows[i]["count_analysis"].ToString();
-        //        parlimen_w.Add(_ParlimentW);
-        //    }
-        //    return parlimen_w;
-        //}
 
-        public static List<Parliment> parlimen(int sid)
+        public static List<Parliment> parlimen_WC(int sid)
         {
             List<Parliment> parliment = new List<Parliment>();
             bus_sis_ugc1 buspar = new bus_sis_ugc1();
