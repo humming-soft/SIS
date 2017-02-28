d9593d2d59fd41d445caacd4be4ac701e6f0534b
agailevictor <agailevictor@gmail.com>
2017-02-28 10:02:30 +0800

    Signed-off-by: agailevictor <agailevictor@gmail.com>

--- SIS2.0/SIS_B/Parliment.cs
+++ SIS2.0/SIS_B/Parliment.cs
@@ -9,10 +9,16 @@ namespace SIS_B
     {
             public string Color_Value { get; set; }
             public int Color_code { get; set; }
-            public int area_id { get; set; }
-            public int area_type { get; set; }
-            public string area_code { get; set; }
-            public string area_name { get; set; }
-            public int parent_id { get; set; }
+            public int par_id { get; set; }
+            public int p_area_type { get; set; }
+            public string par_code { get; set; }
+            public string par_name { get; set; }
+            public int p_parent_id { get; set; }
+            public int dun_id { get; set; }
+            public int d_area_type { get; set; }
+            public string dun_code { get; set; }
+            public string dun_name { get; set; }
+            public int d_parent_id { get; set; }
+            public List<string> dun { get; set; }
     }
 }
