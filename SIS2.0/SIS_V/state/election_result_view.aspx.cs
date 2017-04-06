97df2ec28d30d730d0b77eace02383a0b38d225b
agailevictor <agailevictor@gmail.com>
2017-04-06 10:14:16 +0800

    Signed-off-by: agailevictor <agailevictor@gmail.com>

--- SIS2.0/SIS_V/state/election_result_view.aspx.cs
+++ SIS2.0/SIS_V/state/election_result_view.aspx.cs
@@ -4,14 +4,51 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using SIS_B;
+using System.Data;
+using System.Web.Services;
 
 namespace SIS_V.state
 {
     public partial class election_result_view : System.Web.UI.Page
     {
+        bus_sis_ugc1 bus = new bus_sis_ugc1();
+        DataTable dt;
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                CheckIsLogin();
 
+            }
+        }
+        protected void CheckIsLogin()
+        {
+            if (Session["is_login"] != null)
+            {
+                if (Session["is_login"].ToString() == "t")
+                {
+                    fill_result_list();
+                }
+                else
+                {
+                    Response.Redirect("~/Login");
+                }
+            }
+            else
+            {
+                Response.Redirect("~/Login");
+            }
+        }
+
+        public void fill_result_list()
+        {
+            bus.eid = int.Parse(Session["K_elec_id"].ToString());
+            bus.areaid = int.Parse(Session["K_are_id"].ToString());
+            dt = bus.getDetails();
+            if (dt.Rows.Count > 0)
+            {
+            }
         }
     }
 }
